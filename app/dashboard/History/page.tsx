7d5e91ccae3722757a863db163631ad57e18cd9b
Khalid Abdi <xamza2009@gmail.com>
2025-12-25 00:52:22 +0300

    add shadcn

---
+++ app/dashboard/History/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const History = () => {
+  return (
+    <div className='bg-green-500 w-full'>
+      <h1 className='bg-red-500 w-full'>hi</h1>
+    </div>
+  )
+}
+
+export default History
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 
 import React from 'react'
 import Hello from '../components/page'
+import Page from './dashboard/page'
 
  export default function page () {
   console.log("what type :?")
   return (
     <div>
-      <h1 className='text-4xl underline'>welcome to khalid</h1>
-      <Hello/>
+      <Page/>
     </div>
   )
 }

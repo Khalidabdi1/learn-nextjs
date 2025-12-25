0a117c567ca628d87bae8d71bb347eed3225c282
Khalid Abdi <xamza2009@gmail.com>
2025-12-25 21:45:01 +0300

    edit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <div>
-        <nav>this nav</nav>
+        <nav>this nav test</nav>
       {children}
       <footer>this footer</footer>
     </div>
